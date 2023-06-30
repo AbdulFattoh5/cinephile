cdc8ed6bb32a361453c7985bd57a10e90d4339af
AbdulFattoh5 <abdulfattokh55@gmail.com>
2023-06-30 23:26:03 +0500

    info block started, content changed

---
+++ src/components/InfoBlock/InfoBlock.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="infoblock">
+    <img
+      src="@/assets/images/close.svg"
+      class="infoblock__close"
+      @click="$emit('close')"
+      alt=""
+    />
+  </div>
+</template>
+
+<script setup>
+    const props = defineProps()
+</script>
--- src/components/MOT/Content.vue
+++ src/components/MOT/Content.vue
@@ -1,55 +1,71 @@
 <template>
-    <section class="main__video">
-        <router-link :to="`${props.type}/`" class="main__video-title">
-            <span>{{ props.type == 'movie' ? 'Фильмы' : 'Сереалы' }}</span>
-            <img src="@/assets/images/arrow.png" alt="">
+  <section class="main__video">
+    <router-link :to="`${props.type}/`" class="main__video-title">
+      <span>{{ props.type == "movie" ? "Фильмы" : "Сереалы" }}</span>
+      <img src="@/assets/images/arrow.png" alt="" />
+    </router-link>
+    <Swiper
+      :modules="modules"
+      :space-between="25"
+      :navigation="true"
+      :breakpoints="swiperOption.breakpoints"
+    >
+      <SwiperSlide class="main__video-item" v-for="(item, index) in content">
+        <img
+          v-lazy="imgUrlFull + item.backdrop_path"
+          class="main__video-item-img"
+          src="@/assets/images/card.png"
+          alt=""
+        />
+        <router-link :to="`${props.type}/`" class="main__video-item-link" />
+        <h2 class="main__video-item-title">{{ item.title || item.name }}</h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <router-link :to="`${props.type}/`" class="main__video-item">
+            {{ props.type == "movie" ? "Все фильмы" : "Все сереалы" }}
         </router-link>
-        <Swiper :modules="modules" :space-between="25" :navigation="true" :breakpoints="swiperOption.breakpoints">
-            <SwiperSlide class="main__video-item" v-for="(item, index) in content">
-                <img v-lazy="imgUrlFull + item.backdrop_path" class="main__video-item-img" src="@/assets/images/card.png"
-                    alt="">
-                <router-link :to="`${props.type}/`" class="main__video-item-link" />
-                <h2 class="main__video-item-title">{{ item.title || item.name }}</h2>
-            </SwiperSlide>
-        </Swiper>
-    </section>
+      </SwiperSlide>
+    </Swiper>
+  </section>
 </template>
 
 <script setup>
-import { onMounted, ref, computed } from 'vue';
-import { usePopular } from '@/stores/popular'
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Navigation } from 'swiper'
-import { imgUrlFull } from '@/static';
-import 'swiper/scss'
-import 'swiper/scss/navigation'
+import { onMounted, ref, computed } from "vue";
+import { usePopular } from "@/stores/popular";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation } from "swiper";
+import { imgUrlFull } from "@/static";
+import "swiper/scss";
+import "swiper/scss/navigation";
 
-const props = defineProps(['type'])
-const popular = usePopular()
-let modules = ref([Navigation])
+const props = defineProps(["type"]);
+const popular = usePopular();
+let modules = ref([Navigation]);
 
-const content = computed(() => props.type == 'movie' ? popular.popularMovies : popular.popularTvs)
+const content = computed(() =>
+  props.type == "movie" ? popular.popularMovies : popular.popularTvs
+);
 let swiperOption = ref({
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-        },
-        576: {
-            slidesPerView: 2,
-        },
-        900: {
-            slidesPerView: 3,
-        },
-        1200: {
-            slidesPerView: 4,
-        },
-        1420: {
-            slidesPerView: 5,
-        },
-    }
-})
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1420: {
+      slidesPerView: 5,
+    },
+  },
+});
 
 onMounted(() => {
-    popular.getPopular({ type: props.type })
-})
+  popular.getPopular({ type: props.type });
+});
 </script>
